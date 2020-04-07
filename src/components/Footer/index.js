7c152f7a250397081ab60bda6253ac62bee281e0
Hari Krishnan <62686776+thefitbea@users.noreply.github.com>
2020-04-08 05:10:18 +0530

    Update index.js
    Trying to modify footer from default

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,7 @@ const Footer = () => {
           <p>
             {config.copyright}
           </p>
-          <p>Powered by <a href='https://www.gatsbyjs.org'>Gatsby</a> and <a href='https://www.netlifycms.org'>Netlify CMS</a> | <a href='https://github.com/v4iv/gatsby-starter-business'>Github Repository</a></p>
+          <p>Made with love in Kerala</p><p>Funded by Grandma and cousins in United States</p>
         </div>
       </div>
     </footer>
