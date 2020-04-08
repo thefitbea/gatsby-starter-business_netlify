614d6186d7f3c982a6ba588515801f54adbfea3b
Hari Krishnan <62686776+thefitbea@users.noreply.github.com>
2020-04-08 06:42:27 +0530

    Update config.js
    Theme modified to yellow_black

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Business', // Site title.
-  siteTitleAlt: 'Business', // Alternative site title for SEO.
+  siteTitle: 'Fitbea - Helping people to be fit', // Site title.
+  siteTitleAlt: 'Fitbe- #Be A Fit', // Alternative site title for SEO.
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
+  siteUrl: 'https:/fitbea.com', // Domain of your website without pathPrefix.
   pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
   siteDescription: 'Helping people to be fit.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
-  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
+  disqusShortname: 'fitbea', // Disqus shortname.
   userName: 'Harikrishnan',
   userTwitter: 'fitbea',
   userLocation: 'Kerala. India',
   userDescription: '',
   copyright: 'Copyright © 2020 Fitbea. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#ffe900', // Used for setting manifest and progress theme colors. default is #00d1b2
-  backgroundColor: '#ffffff', // Used for setting manifest background color.
+  backgroundColor: '#000000', // Used for setting manifest background color. default is #ffffff
 }
